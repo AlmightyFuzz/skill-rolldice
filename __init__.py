6ae3558564a7ed53baef0de500e5519c2c6237ea
Tristan <TristanTeasdale@gmail.com>
2017-10-22 19:15:32 +0100

    Added ability to roll more dice
    You can now roll a d4, d6, d8, d10, d12, d20 or d100

--- __init__.py
+++ __init__.py
@@ -9,3 +9,33 @@ def setup(opsdroid):
 async def rolld4(opsdroid, config, message):
     text = "Roll: {}".format(randint(1,4))
     await message.respond(text)
+
+@match_regex(r'rolld6')
+async def rolld6(opsdroid, config, message):
+    text = "Roll: {}".format(randint(1,6))
+    await message.respond(text)
+	
+@match_regex(r'rolld8')
+async def rolld8(opsdroid, config, message):
+    text = "Roll: {}".format(randint(1,8))
+    await message.respond(text)
+	
+@match_regex(r'rolld10\b')
+async def rolld10(opsdroid, config, message):
+    text = "Roll: {}".format(randint(1,10))
+    await message.respond(text)
+	
+@match_regex(r'rolld12')
+async def rolld12(opsdroid, config, message):
+    text = "Roll: {}".format(randint(1,12))
+    await message.respond(text)
+	
+@match_regex(r'rolld20')
+async def rolld20(opsdroid, config, message):
+    text = "Roll: {}".format(randint(1,20))
+    await message.respond(text)
+	
+@match_regex(r'rolld100')
+async def rolld100(opsdroid, config, message):
+    text = "Roll: {}".format(randint(1,100))
+    await message.respond(text)
